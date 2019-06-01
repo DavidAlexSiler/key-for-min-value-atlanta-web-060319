5822a824cc067ed4de059267aee70d4b49ed1c11
Alex Siler <davidalexsiler@gmail.com>
2019-06-01 04:25:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,4 +6,6 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   name_hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
+      lowest_value = v 
+      lowest_key = k 
 end
